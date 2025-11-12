e3a8f00ddb474658fe621cc27f5628293c91db7b
Gábor Csárdi <csardi.gabor@gmail.com>
2025-11-12 10:20:46 +0100

    Copy over common parts from tsjson/tstoml

--- R/ts-package.R
+++ R/ts-package.R
@@ -2,5 +2,10 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @useDynLib ts, .registration = TRUE, .fixes = "c_"
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
 ## usethis namespace: end
 NULL
---
+++ R/utils.R
@@ -0,0 +1,19 @@
+`%||%` <- function(l, r) {
+  if (is.null(l)) {
+    r
+  } else {
+    l
+  }
+}
+
+map_int <- function(.x, .f, ...) {
+  vapply(.x, .f, integer(1), ...)
+}
+
+map_chr <- function(.x, .f, ...) {
+  vapply(.x, .f, character(1), ...)
+}
+
+map_lgl <- function(.x, .f, ...) {
+  vapply(.x, .f, logical(1), ...)
+}
