7d5eae89fbca3d96574aceca3bc9f340537e80b2
Gábor Csárdi <csardi.gabor@gmail.com>
2025-11-27 16:30:30 +0100

    Properly implement ts_dom_tree()
    Using the `$dom_*` columns. It is not the same as the
    syntax tree, even for JSON(C).

--- R/ts-tree-dom.R
+++ R/ts-tree-dom.R
@@ -12,5 +12,25 @@ ts_tree_dom <- function(tree) {
 #' @export
 
 ts_tree_dom.default <- function(tree) {
-  ts_tree_ast(tree)
+  is_dom_node <- c(1L, which(!is.na(tree$dom_parent)))
+  dom <- tree[is_dom_node, ]
+
+  treetab <- data_frame(
+    id = as.character(dom$id),
+    children = lapply(dom$dom_children, as.character),
+    label = paste0(
+      dom$dom_type,
+      " (",
+      dom$id,
+      ")",
+      ifelse(
+        is.na(dom$dom_name),
+        "",
+        cli::col_grey(paste0(" # ", dom$dom_name))
+      )
+    )
+  )
+
+  tree <- cli::tree(treetab)
+  tree
 }
